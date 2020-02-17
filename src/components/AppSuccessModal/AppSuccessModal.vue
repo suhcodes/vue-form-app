67774b13335705a3b99a59a56fa19befd0c99018
suhprog <s.moraes.antunes@gmail.com>
2020-02-16 21:49:54 -0300

    modal success state update

--- src/components/AppSuccessModal/AppSuccessModal.vue
+++ src/components/AppSuccessModal/AppSuccessModal.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="v-overlay v-show">
-    <div class="v-modal">
-      <div class="v-modal__header">
+    <div class="v-modal-success">
+      <div class="v-modal-success__header">
         <router-link to="/list"><button id="closeModal">&times;</button></router-link>
       </div>
-      <div class="v-modal__body">
+      <div class="v-modal-success__body">
         <p>Your app has been saved!</p>
       </div>
-      <div class="v-modal__footer">
+      <div class="v-modal-success__footer">
         <router-link to="/list"><button class="btn btn-success">Ok</button></router-link>
       </div>
     </div>
