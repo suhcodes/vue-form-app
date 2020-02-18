a59b051f4d0fe706004be57c8ae00e43175c4f0b
suhprog <s.moraes.antunes@gmail.com>
2020-02-18 01:51:26 -0300

    edit app list style

--- src/components/AppList/AppList.vue
+++ src/components/AppList/AppList.vue
@@ -2,8 +2,11 @@
   <div class="v-list">
     <h2>Apps List</h2>
     <ul class="v-list__ul" v-if="appList.length">
-      <li class="v-list__ul__item" v-for="app in appList" :key="app.id">
-        <p>{{ app.name }}</p>
+      <li class="v-list__ul__item"
+        v-for="app in appList"
+        :key="app.id"
+        :style="{ borderColor: app.background}">
+        <p><span>{{ app.name }}</span><span>{{ app.category }}</span></p>
       </li>
     </ul>
     <p class="v-list__empty" v-if="!appList.length">Oops!<br /> You don't have any apps yet :(</p>
