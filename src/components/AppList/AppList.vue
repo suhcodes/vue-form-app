f9e83e45113dcce9b3373db4e4f77e1597ec688e
suhprog <s.moraes.antunes@gmail.com>
2020-02-25 21:37:09 -0300

    added remove button and function to apps

--- src/components/AppList/AppList.vue
+++ src/components/AppList/AppList.vue
@@ -8,6 +8,7 @@
         <router-link :to="`view/${app.id}`">
           <p><span>{{ app.name }}</span><span>{{ app.category }}</span></p>
         </router-link>
+        <button @click="clickedToRemove(app.id)"><span>&times;</span></button>
       </li>
     </ul>
     <p class="v-list__empty" v-else>Oops!<br /> You don't have any apps yet :(</p>
@@ -24,14 +25,27 @@ export default {
       hasLoaded: false,
     };
   },
-  async created() {
-    try {
-      const res = await axios.get('http://192.168.1.9:3000/list');
-      this.appList = res.data;
-      this.hasLoaded = true;
-    } catch (e) {
-      console.error(e);
-    }
+  methods: {
+    async clickedToRemove(id) {
+      try {
+        axios.delete(`http://192.168.1.9:3000/list/${id}`);
+        await this.getListApps();
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async getListApps() {
+      try {
+        const res = await axios.get('http://192.168.1.9:3000/list');
+        this.appList = res.data;
+        this.hasLoaded = true;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+  created() {
+    this.getListApps();
   },
 };
 </script>
